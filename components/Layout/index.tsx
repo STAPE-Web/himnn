824f9cbd9bb660a4f7e2ceb760142ef22739a3e2
Kirill Kirilenko <ggggfetfdfgjjh2@gmail.com>
2024-02-17 12:54:59 +0500

    update

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import React, { FC } from 'react'
-import Header from '@/components/Header'
+// import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import SearchBox from '@/components/SearchBox'
 import OrderCall from '../OrderCall'
+import dynamic from 'next/dynamic';
+const Header = dynamic(() => import('@/components/Header'), {
+    ssr: false
+});
 
 interface Props {
     children: any
