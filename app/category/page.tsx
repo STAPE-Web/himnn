951c46f626c65c926f584bd85313f1cc7589f4c6
STAPE <ggggfetfdfgjjh2@gmail.com>
2024-04-07 12:41:35 +0500

    update

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,44 +1,50 @@
-"use client"
+"use client";
 
-import Bread from "@/components/Bread"
-import Filter from "@/components/Filter"
-import Layout from "@/components/Layout"
-import styles from "./style.module.css"
-import CategoryItems from "@/components/CategoryItems"
-import CatalogItems from "@/components/CatalogItems"
-import { Suspense, useEffect, useState } from "react"
+import Bread from "@/components/Bread";
+import Filter from "@/components/Filter";
+import Layout from "@/components/Layout";
+import styles from "./style.module.css";
+import CategoryItems from "@/components/CategoryItems";
+import CatalogItems from "@/components/CatalogItems";
+import { Suspense, useEffect, useState } from "react";
 
 const Category = () => {
-  const [filterData, setFilterData] = useState<string[]>([])
-  const [items, setItems] = useState<string[]>(["В наличии (100)"])
-  const [min, setMin] = useState("10")
-  const [max, setMax] = useState("1000")
+  const [filterData, setFilterData] = useState<string[]>([]);
+  const [items, setItems] = useState<string[]>(["В наличии (100)"]);
+  const [min, setMin] = useState("10");
+  const [max, setMax] = useState("1000");
 
-  const [category, setCategory] = useState("")
-  const [subcategory, setsubcategory] = useState("")
-  const [categoryTitle, setcategoryTitle] = useState("")
-  const [subcategoryTitle, setsubcategoryTitle] = useState("")
+  const [category, setCategory] = useState("");
+  const [subcategory, setsubcategory] = useState("");
+  const [categoryTitle, setcategoryTitle] = useState("");
+  const [subcategoryTitle, setsubcategoryTitle] = useState("");
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const searchParams = new URLSearchParams(window.location.search);
-      setCategory(searchParams.get('c') || "")
-      setsubcategory(searchParams.get('sub') || "")
-      setcategoryTitle(category ? category.replace(/-/g, ' ') : "")
-      setsubcategoryTitle(subcategory ? subcategory.replace(/-/g, ' ') : "")
+      setCategory(searchParams.get("c") || "");
+      setsubcategory(searchParams.get("sub") || "");
+      setcategoryTitle(category ? category.replace(/-/g, " ") : "");
+      setsubcategoryTitle(subcategory ? subcategory.replace(/-/g, " ") : "");
     }
-  }, [category, subcategory, categoryTitle, subcategoryTitle])
+  }, [category, subcategory, categoryTitle, subcategoryTitle]);
 
-  const bread = subcategory === "" ? [
-    { link: "/", name: "Главная" },
-    { link: "/catalog", name: "Каталог" },
-    { link: `/category?c=${category}`, name: categoryTitle },
-  ] : [
-    { link: "/", name: "Главная" },
-    { link: "/catalog", name: "Каталог" },
-    { link: `/category?c=${category}`, name: categoryTitle },
-    { link: `/category?c=${category}&sub=${subcategory}`, name: subcategoryTitle },
-  ]
+  const bread =
+    subcategory === ""
+      ? [
+          { link: "/", name: "Главная" },
+          { link: "/catalog", name: "Каталог" },
+          { link: `/category?c=${category}`, name: categoryTitle },
+        ]
+      : [
+          { link: "/", name: "Главная" },
+          { link: "/catalog", name: "Каталог" },
+          { link: `/category?c=${category}`, name: categoryTitle },
+          {
+            link: `/category?c=${category}&sub=${subcategory}`,
+            name: subcategoryTitle,
+          },
+        ];
 
   return (
     <Layout>
@@ -46,20 +52,41 @@ const Category = () => {
         <section className={styles.Section}>
           <Bread array={bread} />
 
-          <h2>{categoryTitle}</h2>
+          <h2>{subcategoryTitle === "" ? categoryTitle : subcategoryTitle}</h2>
 
           <div className={styles.Row}>
-            <Filter filterData={filterData} setFilterData={setFilterData} max={max} min={min} setMax={setMax} setMin={setMin} items={items} setItems={setItems} />
+            <Filter
+              filterData={filterData}
+              setFilterData={setFilterData}
+              max={max}
+              min={min}
+              setMax={setMax}
+              setMin={setMin}
+              items={items}
+              setItems={setItems}
+            />
 
             <div className={styles.Items}>
-              {filterData.length === 0 && <CategoryItems category={category as string} subcategory={subcategory as string} />}
-              <CatalogItems category={category as string} subcategory={subcategory as string} filterData={filterData} items={items} min={min} max={max} />
+              {filterData.length === 0 && (
+                <CategoryItems
+                  category={category as string}
+                  subcategory={subcategory as string}
+                />
+              )}
+              <CatalogItems
+                category={category as string}
+                subcategory={subcategory as string}
+                filterData={filterData}
+                items={items}
+                min={min}
+                max={max}
+              />
             </div>
           </div>
         </section>
       </Suspense>
     </Layout>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- components/CategoryItems/index.tsx
+++ components/CategoryItems/index.tsx
@@ -1,44 +1,70 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import styles from "./style.module.css"
-import { useRouter } from "next/navigation"
-import { FC, useEffect, useState } from "react"
-import { ICategory } from "@/types"
-import { CategoriesAPI } from "@/api"
+import Image from "next/image";
+import styles from "./style.module.css";
+import { useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
+import { ICategory } from "@/types";
+import { CategoriesAPI } from "@/api";
 
 interface Props {
-    category: string
-    subcategory: string
+  category: string;
+  subcategory: string;
 }
 
 const CategoryItems: FC<Props> = ({ category, subcategory }) => {
-    const router = useRouter()
-    const [data, setData] = useState<ICategory[]>([])
+  const router = useRouter();
+  const [data, setData] = useState<ICategory[]>([]);
 
-    async function getAllCatalogs() {
-        const result = await CategoriesAPI.getAll()
-        setData(result)
-    }
+  async function getAllCatalogs() {
+    const result = await CategoriesAPI.getAll();
+    setData(result);
+  }
 
-    useEffect(() => {
-        getAllCatalogs()
-    }, [])
+  useEffect(() => {
+    getAllCatalogs();
+  }, []);
 
-    return (
-        <div className={`${styles.List} ${subcategory !== "" ? styles.Hidden : ""} ${data.filter(i => i.data.category.toLowerCase() === category.toLowerCase().replace(/-/g, ' ')).length === 0 ? styles.Hidden : ""}`}>
-            {data.filter(i => i.data.category.toLowerCase() === category.toLowerCase().replace(/-/g, ' ')).map((item, index) => (
-                <div key={index} className={styles.Item} onClick={() => router.push(`/category?c=${category}&sub=${item.data.title.toLowerCase().replace(/ /g, '-')}`)}>
-                    <Image src={item.data.image} width={220} height={220} alt="" />
+  return (
+    <div
+      className={`${styles.List} ${subcategory !== "" ? styles.Hidden : ""} ${
+        data.filter(
+          (i) =>
+            i.data.category.toLowerCase() ===
+            category.toLowerCase().replace(/-/g, " ")
+        ).length === 0
+          ? styles.Hidden
+          : ""
+      }`}
+    >
+      {data
+        .filter(
+          (i) =>
+            i.data.category.toLowerCase() ===
+            category.toLowerCase().replace(/-/g, " ")
+        )
+        .map((item, index) => (
+          <div
+            key={index}
+            className={styles.Item}
+            onClick={() =>
+              router.push(
+                `/category?c=${category}&sub=${item.data.title
+                  .toLowerCase()
+                  .replace(/ /g, "-")}`
+              )
+            }
+          >
+            <Image src={item.data.image} width={220} height={220} alt="" />
 
-                    <div>
-                        <h3>{item.data.title}</h3>
-                        <p>{item.data.text}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    )
-}
+            <div>
+              <h3>{item.data.title}</h3>
+              <p>{item.data.text}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default CategoryItems
+export default CategoryItems;
