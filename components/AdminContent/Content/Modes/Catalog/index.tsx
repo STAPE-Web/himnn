41ef6b89cbd6e0a5b1bbdd8e6cd0110467c7abb3
Kirill Kirilenko <72465053+STAPE-Web@users.noreply.github.com>
2024-04-27 13:56:26 +0500

    Update index.tsx

--- components/AdminContent/Content/Modes/Catalog/index.tsx
+++ components/AdminContent/Content/Modes/Catalog/index.tsx
@@ -51,7 +51,7 @@ const Catalog = () => {
             {data.map((item, index) => (
                 <div key={index} className={styles.Item}>
                     <div className={styles.ItemBox}>
-                        <Image src={item.data.image} width={100} height={100} alt="" />
+                        <img src={item.data.image} width={100} height={100} alt="" />
 
                         <div className={styles.Data}>
                             <h3>{item.data.title}</h3>
